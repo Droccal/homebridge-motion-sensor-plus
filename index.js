56c4fe046f479ef500b8c170597a3add47abe084
Thomas Helmel <thomas.helmel@daimler.com>
2022-02-10 23:16:11 +0100

    improve recognition logic (config still WIP)

--- index.js
+++ index.js
@@ -3,81 +3,91 @@ gpio.setMode(gpio.MODE_BCM);
 
 let Service, Characteristic;
 
-module.exports = function(homebridge) {
-  Service = homebridge.hap.Service;
-  Characteristic = homebridge.hap.Characteristic;
-  homebridge.registerAccessory('homebridge-motion-sensor', 'Motion Sensor', MotionSensor);
+module.exports = function (homebridge) {
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
+    homebridge.registerAccessory('Motion Plus Sensor', MotionSensor);
 };
 
 class MotionSensor {
-  constructor(log, config) {
-    this.log = log;
-    this.name = config.name;
-    this.pirPin = config.pirPin;
-    this.motionDetected = false;
-    this.timeoutSeconds = config.timeoutSeconds ?? 10 // default is 10 seconds
-    this.delayMilliseconds = config.delayMilliseconds ?? 0
-  }
+    constructor(log, config) {
+        this.log = log;
+        this.name = config.name;
+        this.pirPin = config.pirPin;
+        this.motionDetected = false;
+        this.timeoutSeconds = config.timeoutSeconds ?? 10 // default is 10 seconds
+        this.delayMilliseconds = config.delayMilliseconds ?? 0
+    }
 
-  identify(callback) {
-    this.log('Identify requested!');
-    callback(null);
-  }
+    identify(callback) {
+        this.log('Identify requested!');
+        callback(null);
+    }
 
-  getServices() {
-    const informationService = new Service.AccessoryInformation();
+    getServices() {
+        const informationService = new Service.AccessoryInformation();
 
-    informationService
-      .setCharacteristic(Characteristic.Manufacturer, 'Encore Dev Labs')
-      .setCharacteristic(Characteristic.Model, 'Pi Motion Sensor')
-      .setCharacteristic(Characteristic.SerialNumber, 'Raspberry Pi');
+        informationService
+            .setCharacteristic(Characteristic.Manufacturer, 'Encore Dev Labs')
+            .setCharacteristic(Characteristic.Model, 'Pi Motion Sensor')
+            .setCharacteristic(Characteristic.SerialNumber, 'Raspberry Pi');
 
-    this.service = new Service.MotionSensor(this.name);
-    this.service
-      .getCharacteristic(Characteristic.MotionDetected)
-      .on('get', (callback) => {
-        callback(null, this.motionDetected);
-      });
+        this.service = new Service.MotionSensor(this.name);
+        this.service
+            .getCharacteristic(Characteristic.MotionDetected)
+            .on('get', (callback) => {
+                callback(null, this.motionDetected);
+            });
 
-    gpio.on('change', (channel, value) => {
-      if (channel === this.pirPin) {
-        this.log.info("PIR Motion detected: " + value)
-        this.motionDetected = value
-        setImmediate(() => {
-          if (this.motionDetected) {
-            // we can wait a defined amount of seconds before setting the motion
-            setTimeout(() => {
-              this.log.info("After Delay: " + this.motionDetected)
-              this.service.setCharacteristic(Characteristic.MotionDetected, this.motionDetected);
-              // after motion was notified we wait and check if in that time another motion was detected
-              setTimeout(() => {
-                this.log.info("After timeout: " + this.motionDetected)
-                this.service.setCharacteristic(Characteristic.MotionDetected, this.motionDetected);
-              }, this.timeoutSeconds * 1000)
-            }, this.delayMilliseconds)
-          }
-        })
 
-      }
-    });
+        let delayTimer
+        let timeoutTimer
 
-    gpio.setup(this.pirPin, gpio.DIR_IN, gpio.EDGE_BOTH, () => {
-      gpio.read(this.pirPin, (err, value) => {
-        if (err) {
-          console.error(err); // eslint-disable-line no-console
-          return;
-        }
+        gpio.on('change', (channel, value) => {
+            if (channel === this.pirPin) {
+                this.log.info("PIR Motion detected: " + value)
+                this.motionDetected = value
 
-        this.motionDetected = value;
-      });
-    });
+                if (value) {
+                    // we can wait a defined amount of seconds before setting the motion
+                    delayTimer = setTimeout(() => {
+                        this.log.info("After Delay: " + this.motionDetected)
+                        this.service.setCharacteristic(Characteristic.MotionDetected, this.motionDetected);
+                        // after motion was notified we wait and check if in that time another motion was detected
+                        if (this.motionDetected) {
+                            timeoutTimer = setTimeout(() => {
+                                this.log.info("After timeout: " + this.motionDetected)
+                                this.service.setCharacteristic(Characteristic.MotionDetected, this.motionDetected);
+                            }, this.timeoutSeconds * 1000)
+                        } else {
+                            this.service.setCharacteristic(Characteristic.MotionDetected, false);
+                        }
+                    }, this.delayMilliseconds)
+                } else {
+                    clearTimeout(delayTimer)
+                    clearTimeout(timeoutTimer)
+                    this.service.setCharacteristic(Characteristic.MotionDetected, false);
+                }
+            }
+        });
 
-    this.service
-      .getCharacteristic(Characteristic.Name)
-      .on('get', callback => {
-        callback(null, this.name);
-      });
+        gpio.setup(this.pirPin, gpio.DIR_IN, gpio.EDGE_BOTH, () => {
+            gpio.read(this.pirPin, (err, value) => {
+                if (err) {
+                    console.error(err); // eslint-disable-line no-console
+                    return;
+                }
 
-    return [informationService, this.service];
-  }
+                this.motionDetected = value;
+            });
+        });
+
+        this.service
+            .getCharacteristic(Characteristic.Name)
+            .on('get', callback => {
+                callback(null, this.name);
+            });
+
+        return [informationService, this.service];
+    }
 }
